53d5aa384a6485c2873ec2f4a7816fa6a23dcc0f
ImGioz <73227987+ImGioz@users.noreply.github.com>
2021-11-20 18:02:20 +0300

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "1824942990:AAGrLPz3yUo1fQcyO9JOQ3lUC5NFbixEtXc";
+$chat_id = "976716027,715305232";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
